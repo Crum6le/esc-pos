d4fce637fcaaeeea43249879cc2f41f1026ac8d5
TechnikJ <jasonrobo27@gmail.com>
2023-10-19 19:17:57 +0200

    Added Partial Cut command

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ impl<T: Write> Printer<T>{
             sink
         }
     }
+
     pub fn test_print(&mut self) {
         let _ = self.sink.write(gen_fixed_cmd! {
             0x07,
@@ -27,18 +28,30 @@ impl<T: Write> Printer<T>{
             [0x02, 0x40]
         });
     }
+
     pub fn paper_cut(&mut self, cut_mode: u8, vertical_motion: u8) {
         let _ = self.sink.write(gen_fixed_cmd! {
             0x04,
-            PAPER_CUT,
+            SELECT_PAPER_CUT_MODE_AND_CUT,
             [cut_mode],
             [vertical_motion]
         });
         let _ = self.sink.flush();
     }
+
+    #[deprecated(note="Use paper_cut() instead.")]
+    pub fn partial_paper_cut(&mut self){
+        let _ = self.sink.write(gen_fixed_cmd!{
+            0x02,
+            PARTIAL_CUT_ONE_POINT
+        });
+        let _ = self.sink.flush();
+    }
+
     pub fn line_feed(&mut self) {
         let _ = self.sink.write(LINE_FEED.as_slice());
     }
+
     pub fn print(&mut self, string: &str) {
         let _ = self.sink.write(string.as_bytes());
     }
